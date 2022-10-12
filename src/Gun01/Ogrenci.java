f02d0caf70332ac0365ed7c7322364e777d64b7a
kilicma <iam.mehmetalikilic@gmail.com>
2022-10-12 09:44:23 +0300

    1. adım yapıldı

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -5,5 +5,12 @@ public class Ogrenci {
         for (int i = 0; i < 5; i++) {
             System.out.println("merhaba");
         }
+        int secim=0;
+        while (secim<5)
+        {
+            System.out.println("merhaba");
+            secim++;
+        }
     }
+
 }
